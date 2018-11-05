a4c189da56c8cefa2f938ca5c289044281efa8ca
Ricardo <ricardosaracino@protonmail.com>
2018-11-04 20:31:51 -0500

    valuesApi

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -11,22 +11,21 @@ namespace IdentityServer
         {
             return new List<ApiResource>
             {
-                new ApiResource("api1", "My API")
+                new ApiResource("valuesApi", "My API")
             };
         }
 
         public static IEnumerable<Client> GetClients() => new List<Client>
         {
             // server to server
-            // http://docs.identityserver.io/en/release/quickstarts/6_aspnet_identity.html
-            // https://github.com/IdentityServer/IdentityServer4.Demo/blob/master/src/IdentityServer4Demo/Config.cs
+
             new Client
             {
                 ClientId = "client",
                 ClientSecrets = { new Secret("secret".Sha256()) },
 
                 AllowedGrantTypes = GrantTypes.ClientCredentials,
-                AllowedScopes = { "api1" },
+                AllowedScopes = { "valuesApi" },
             }
         };
     }
---
+++ IdentityServer/README.md
@@ -0,0 +1,4 @@
+http://docs.identityserver.io/en/release/quickstarts/6_aspnet_identity.html
+https://github.com/IdentityServer/IdentityServer4.Demo/blob/master/src/IdentityServer4Demo/Config.cs
+https://www.linkedin.com/pulse/securing-net-core-web-api-identityserver4-using-owner-dalvandi?published=t
+https://www.npmjs.com/package/simple-oauth2
